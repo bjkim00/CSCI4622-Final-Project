c8cec3a9c4151d9069dfca9e7005914cb8920be9
adamhoerger <adam.hoerger@gmail.com>
2020-12-04 16:44:03 -0700

    Fixed some indexing

--- DataHandler.py
+++ DataHandler.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 
 class DataHandler():
-    def __init__(self, beg = 1999, end = 2019,offset = 17,  split_by_pos = False,ignore_na = True, test_size = 0.2, min_num = 50):
+    def __init__(self, beg = 1999, end = 2019,offset = 17,  split_by_pos = False, ignore_na = True, fill_mean = False, test_size = 0.2, min_num = 50):
         self.stats = ['PassingYds', 'PassingTD', 'Int', 'PassingAtt', 'Cmp', \
                       'RushingAtt', 'RushingYds', 'RushingTD', 'Rec', 'Tgt', 'ReceivingYds', 'ReceivingTD', 'FL']
         years = range(beg, end)
@@ -20,6 +20,11 @@ class DataHandler():
             data[[s + '-' + str(i) for s in self.stats]] = data.groupby('Player')[self.stats].shift(i)
         if ignore_na:
             data = data.dropna()
+        else:
+            if fill_mean:
+                data = data.fillna(data.mean())
+            else:
+                data = data.fillna(0)
             
         data = data.reset_index()
         cols = data.columns.tolist()
@@ -29,18 +34,18 @@ class DataHandler():
         cols.remove("Year")
         cols.remove("Week")
         
-        cols = ["Year", "Week"] + cols[1:-3]
+        cols = ["Year", "Week"] + cols[1:]
         data = data[cols]
 
         if split_by_pos:
             self.data = {}
-            self.names_train, self.names_test, self.X_train, self.X_test, self.y_train, self.y_test = {}, {}, {}, {}, {}, {}
+            self.info_train, self.info_test, self.X_train, self.X_test, self.y_train, self.y_test = {}, {}, {}, {}, {}, {}
             for pos in data.Pos.unique():
                 if len(data[data['Pos'] == pos]) >= min_num:
                     self.data[pos] = data[data['Pos'] == pos]
-                    names, X, y = self.data[pos].iloc[:, 2], self.data[pos].iloc[:, 18:], self.data[pos].iloc[:, 5:17]
-                    self.names_train[pos], self.names_test[pos], self.X_train[pos], self.X_test[pos], self.y_train[pos], self.y_test[pos] = train_test_split(names, X, y, test_size = test_size)   
+                    info, X, y = self.data[pos].iloc[:, :5], self.data[pos].iloc[:, 18:], self.data[pos].iloc[:, 5:18]
+                    self.info_train[pos], self.info_test[pos], self.X_train[pos], self.X_test[pos], self.y_train[pos], self.y_test[pos] = train_test_split(info, X, y, test_size = test_size)   
         else:
             self.data = data
-            names, X, y = self.data.iloc[:, 2], self.data.iloc[:, 18:], self.data.iloc[:, 5:17]
-            self.names_train, self.names_test, self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(names, X, y, test_size = test_size)
+            info, X, y = self.data.iloc[:, :5], self.data.iloc[:, 18:], self.data.iloc[:, 5:18]
+            self.info_train, self.info_test, self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(names, X, y, test_size = test_size)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
 from DataHandler import DataHandler
-dh = DataHandler(beg = 2015, split_by_pos=True)
-print(dh.names_train)
+dh = DataHandler(beg=2010, split_by_pos=True, offset=3, ignore_na = False, fill_mean = True)
+print(dh.y_train)
+print(dh.info_train)
